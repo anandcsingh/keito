6e0eed993b9d6131968e231f42f29066e398f2ef
anandcsingh <anandcsingh@gmail.com>
2022-12-14 01:41:56 -0400

    feat: add martial art rank

--- ui/components/sections/RankTiles.js
+++ ui/components/sections/RankTiles.js
@@ -1,33 +1,61 @@
 import Link from 'next/link';
 import React from 'react';
 import RankItem from './partials/RankItem';
+import { useEffect, useState } from "react";
 
 const RankTiles = () => {
-    const items = [
-        { certified: true, shortName: "ibjjf", rank: "Red Belt", martialArt: "Brazilian Jiu Jitsu" },
-        { certified: false, shortName: "itf", rank: "Green Belt", martialArt: "Taekwon-Do" },
-        { certified: true, shortName: "wkf", rank: "Yellow Belt", martialArt: "Karate" }
-    ];
-     const tiles = items.map((i,index) =>
-         <RankItem key={index} martialArtShortName={i.shortName} rank={i.rank} martialArt={i.martialArt} certified={i.certified} />
-    );
+
+    const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [items, setItems] = useState([]);
+  var address = "B62qpzAWcbZSjzQH9hiTKvHbDx1eCsmRR7dDzK2DuYjRT2sTyW9vSpR";
+
+    // Note: the empty deps array [] means
+  // this useEffect will run once
+  // similar to componentDidMount()
+  useEffect(() => {
+
+    
+    fetch(`api/user/${address}`)
+      .then(res => res.json())
+      .then(
+        (result) => {
+          setIsLoaded(true);
+          setItems(result.martialArts);
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        (error) => {
+          setIsLoaded(true);
+          setError(error);
+        }
+      )                                     
+  }, [])
+    
+ 
 
     return (
         <section className="section">
             <div className="container">
                 <div className="section-inner">
                     <div className="tiles-wrap">
-                         {tiles} 
-                         <Link href="addrank">
-                        <div className="tiles-item" title='Add new Martial Art'>
-                            <div className="ma-add-btn">
-                                <div className='pl-8 pt-8 text-sm'>
-                                    <div>
+                        {items.map((i, index) => (
+                    <RankItem key={index} martialArtShortName={i.martialArtShortName} rank={i.rank} martialArt={i.martialArt} certified={i.certified} />
+                        
+                        ))}
 
+
+                        <Link href="addrank">
+                            <div className="tiles-item" title='Add new Martial Art'>
+                                <div className="ma-add-btn">
+                                    <div className='pl-8 pt-8 text-sm'>
+                                        <div>
+
+                                        </div>
                                     </div>
                                 </div>
                             </div>
-                        </div>
                         </Link>
                     </div>
                 </div>
